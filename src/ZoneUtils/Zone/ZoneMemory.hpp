feaf66deda7ca7b8d0fac6bcb9dee0dabddab6e9
RektInator <mlmmlangeveld@gmail.com>
2019-11-03 03:33:06 +0100

    adjust zonememory detail message

--- src/ZoneUtils/Zone/ZoneMemory.hpp
+++ src/ZoneUtils/Zone/ZoneMemory.hpp
@@ -10,128 +10,6 @@
 
 namespace ZoneTool
 {
-#ifdef USE_CPP_POINTERS
-	class ZoneMemPtrInternal {};
-	template <typename T> class ZoneMemPtr : public ZoneMemPtrInternal
-	{
-	private:
-		std::shared_ptr < T >	m_ptr;
-
-	public:
-		ZoneMemPtr()
-		{
-			this->m_ptr = std::make_shared < T >();
-		}
-		template <typename ...Args> ZoneMemPtr(Args&&... args)
-		{
-			this->m_ptr = std::make_shared < T >(args...);
-		}
-		std::shared_ptr < T >& ToShared()
-		{
-			return this->m_ptr;
-		}
-	};
-	template <typename T> class ZoneMemPtrArr : public ZoneMemPtrInternal
-	{
-	private:
-		std::shared_ptr < T >	m_ptr;
-
-	public:
-		ZoneMemPtrArr(std::size_t size)
-		{
-			this->m_ptr = std::shared_ptr < T >(new T[size], std::default_delete<T[]>());
-		}
-		std::shared_ptr < T >& ToShared()
-		{
-			return this->m_ptr;
-		}
-	};
-
-	class ZoneMemory
-	{
-	private:
-		std::vector < ZoneMemPtrInternal >	m_pointers;
-		std::mutex							mutex_;
-
-	public:
-		ZoneMemory()
-		{
-		}
-
-		char* StrDup(const char* name)
-		{
-			std::lock_guard < std::mutex > g(this->mutex_);
-
-			// dup string
-			auto pointer = ZoneMemPtrArr < char >(strlen(name) + 1);
-			strcpy(
-				reinterpret_cast<char*>(pointer.ToShared().get()), 
-				name);
-			this->m_pointers.push_back(pointer);
-
-			// return pointer
-			return reinterpret_cast<char*>(pointer.ToShared().get());
-		}
-		char* StrDup(const std::string &name)
-		{
-			return this->StrDup(name.data());
-		}
-
-		template <typename T> T* Alloc(std::size_t count)
-		{
-			std::lock_guard < std::mutex > g(this->mutex_);
-
-			// alloc pointer and zero it out
-			auto pointer = ZoneMemPtrArr < T >(count);
-			memset(
-				reinterpret_cast<T*>(pointer.ToShared().get()), 
-				0, 
-				sizeof T * count);
-
-			// add pointer to array
-			this->m_pointers.push_back(pointer);
-
-			// return pointer
-			return reinterpret_cast<T*>(pointer.ToShared().get());
-		}
-
-		template <typename T> T* Alloc()
-		{
-			std::lock_guard < std::mutex > g(this->mutex_);
-
-			// alloc pointer and zero it out
-			auto pointer = ZoneMemPtr < T >();
-			memset(
-				reinterpret_cast<T*>(pointer.ToShared().get()),
-				0,
-				sizeof T);
-
-			// add pointer to array
-			this->m_pointers.push_back(pointer);
-
-			// return pointer
-			return reinterpret_cast<T*>(pointer.ToShared().get());
-		}
-
-		template <typename T> T* ManualAlloc(std::size_t size, std::size_t count = 1)
-		{
-			std::lock_guard < std::mutex > g(this->mutex_);
-
-			// alloc pointer and zero it out
-			auto pointer = ZoneMemPtrArr < char >(size * count); // reinterpret_cast<T*>(malloc(size * count));
-			memset(
-				reinterpret_cast<char*>(pointer.ToShared().get()), 
-				0, 
-				size * count);
-
-			// add pointer to array
-			this->m_pointers.push_back(pointer);
-
-			// return pointer
-			return reinterpret_cast<T*>(pointer.ToShared().get());
-		}
-	};
-#else
 	class ZoneMemory
 	{
 	private:
@@ -169,7 +47,7 @@ namespace ZoneTool
 			std::lock_guard<std::recursive_mutex> g(this->mutex_);
 			VirtualFree(memory_pool_, memory_size_, MEM_DECOMMIT | MEM_RELEASE);
 
-			printf("ZoneTool memory statistics: build used %ub of ram (%fmb).\n", mem_pos_,
+			printf("ZoneTool memory statistics: used %ub of ram (%fmb).\n", mem_pos_,
 				static_cast<float>(mem_pos_) / 1024 / 1024);
 
 			memory_pool_ = nullptr;
@@ -229,5 +107,4 @@ namespace ZoneTool
 			return reinterpret_cast<T*>(pointer);
 		}
 	};
-#endif
 }

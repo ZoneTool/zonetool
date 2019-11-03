ce7ed56900699da57c57c5952c290ce3898223bb
RektInator <mlmmlangeveld@gmail.com>
2019-11-03 01:54:49 +0100

    fix FX dumping

--- src/IW3/Assets/FxEffectDef.cpp
+++ src/IW3/Assets/FxEffectDef.cpp
@@ -15,12 +15,12 @@ namespace ZoneTool
 	{
 		void IFxEffectDef::dump(FxEffectDef* asset, ZoneMemory* mem)
 		{
-			const auto iw4_fx = new IW4::FxEffectDef;
+			const auto iw4_fx = mem->Alloc<IW4::FxEffectDef>();
 			memcpy(iw4_fx, asset, sizeof IW4::FxEffectDef);
 
 			const auto elem_count = iw4_fx->elemDefCountEmission + iw4_fx->elemDefCountOneShot + iw4_fx->elemDefCountLooping;
-			iw4_fx->elemDefs = new IW4::FxElemDef[elem_count];
-
+			iw4_fx->elemDefs = mem->Alloc<IW4::FxElemDef>(elem_count);
+			
 			for (auto i = 0; i < elem_count; i++)
 			{
 				memcpy(&iw4_fx->elemDefs[i], &asset->elemDefs[i], sizeof IW4::FxElemDef);
@@ -32,7 +32,17 @@ namespace ZoneTool
 
 				iw4_fx->elemDefs[i].collBounds.compute(iw4_fx->elemDefs[i].collBounds.midPoint, iw4_fx->elemDefs[i].collBounds.halfSize);
 
-				if (iw4_fx->elemDefs[i].elemType != 3 && iw4_fx->elemDefs[i].extended.trailDef)
+				if (iw4_fx->elemDefs[i].elemType == 3 && iw4_fx->elemDefs[i].extended.trailDef)
+				{
+					iw4_fx->elemDefs[i].extended.trailDef = mem->Alloc<IW4::FxTrailDef>();
+					iw4_fx->elemDefs[i].extended.trailDef->scrollTimeMsec = asset->elemDefs[i].trailDef->scrollTimeMsec;
+					iw4_fx->elemDefs[i].extended.trailDef->repeatDist = asset->elemDefs[i].trailDef->repeatDist;
+					iw4_fx->elemDefs[i].extended.trailDef->vertCount = asset->elemDefs[i].trailDef->vertCount;
+					iw4_fx->elemDefs[i].extended.trailDef->verts = reinterpret_cast<IW4::FxTrailVertex*>(asset->elemDefs[i].trailDef->verts);
+					iw4_fx->elemDefs[i].extended.trailDef->indCount = asset->elemDefs[i].trailDef->indCount;
+					iw4_fx->elemDefs[i].extended.trailDef->inds = asset->elemDefs[i].trailDef->inds;
+				}
+				else
 				{
 					iw4_fx->elemDefs[i].extended.trailDef = nullptr;
 				}

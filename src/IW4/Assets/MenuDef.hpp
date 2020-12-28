a759b82ec557e61122f99b8a4b2d928abd9827f6
RektInator <7003455+RektInator@users.noreply.github.com>
2020-12-28 13:36:23 +0100

    fix credits

--- src/IW4/Assets/MenuDef.hpp
+++ src/IW4/Assets/MenuDef.hpp
@@ -3,7 +3,7 @@
 // Call of Duty titles. 
 //
 // Project: https://github.com/ZoneTool/zonetool
-// Author: RektInator (https://github.com/RektInator)
+// Author: aerosoul (https://github.com/RektInator)
 // License: GNU GPL v3.0
 // ========================================================
 #pragma once
